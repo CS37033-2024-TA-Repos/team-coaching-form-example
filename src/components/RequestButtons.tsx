4ea2766068a18ea0401a6c13ebe3bc37295ed3ca
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-01-21 13:47:48 -0500

    finished front end functionality.

--- src/components/RequestButtons.tsx
+++ src/components/RequestButtons.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './RequestButtons.css';
+import {RequestButtonsInterface} from "../common/RequestButtonsInterface";
 
-function RequestButtons() {
+function RequestButtons({submit, clear}: RequestButtonsInterface) {
+    function handleSubmit() {
+        submit();
+    }
+
+    function handleClear() {
+        clear();
+    }
     return (
         <div className={"requestButtonsDiv"}>
+            <button onClick={handleSubmit}>Submit</button>
+            <button onClick={handleClear}>Clear</button>
+            <a href={"/"}>
+                <button>Back</button>
+            </a>
         </div>
     );
 }
--- src/routes/EmployeeFeedbackForm.tsx
+++ src/routes/EmployeeFeedbackForm.tsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './EmployeeFeedbackForm.css';
 import RequestButtons from "../components/RequestButtons";
+import {EmployeeFeedback} from "../common/EmployeeFeedback";
 
 function EmployeeFeedbackForm() {
+    const [name, setName] = useState("");
+    const [comment, setComment] = useState("");
+
+    function submit() {
+        if (name !== "" && comment !== "") {
+            const feedback: EmployeeFeedback = {name: name, comment: comment};
+            console.log(feedback);
+            clear();
+        }
+    }
+
+    function clear() {
+        setName("");
+        setComment("");
+    }
+
+    function handleNameInput(e: ChangeEvent<HTMLInputElement>) {
+        setName(e.target.value);
+    }
+
+    function handleCommentInput(e: ChangeEvent<HTMLTextAreaElement>) {
+        setComment(e.target.value);
+    }
 
 
-
-    return (
-        <div className="SanitationDiv">
-            <h1>Employee Feedback Form</h1>
-            <div className={"formDiv"}>
-                <div className={"inputDiv"}>
-
-                </div>
-                <RequestButtons />
-            </div>
-        </div>
-    );
+  return (
+      <div className="SanitationDiv">
+          <h1>Employee Feedback Form</h1>
+          <div className={"formDiv"}>
+              <div className={"inputDiv"}>
+                  <p>Name:</p>
+                  <input onChange={handleNameInput} value={name}/>
+                  <p>Comment:</p>
+                  <textarea className={"comment"} onChange={handleCommentInput} value={comment}/>
+              </div>
+              <RequestButtons submit={submit} clear={clear}/>
+          </div>
+      </div>
+  );
 }
 
 export default EmployeeFeedbackForm;
