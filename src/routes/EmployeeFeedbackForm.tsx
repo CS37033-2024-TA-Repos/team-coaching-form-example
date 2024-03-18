9886823073521aedc66952a50fe0bebcb48c7129
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-03-18 17:24:47 -0400

    simplified starter code

--- src/routes/EmployeeFeedbackForm.tsx
+++ src/routes/EmployeeFeedbackForm.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import './EmployeeFeedbackForm.css';
-import RequestButtons from "../components/RequestButtons";
 
 function EmployeeFeedbackForm() {
 
 
 
     return (
-        <div className="SanitationDiv">
-            <h1>Employee Feedback Form</h1>
-            <div className={"formDiv"}>
-                <div className={"inputDiv"}>
-
-                </div>
-                <RequestButtons />
-            </div>
-        </div>
+        <h1>Employee Feedback Form</h1>
     );
 }
 
