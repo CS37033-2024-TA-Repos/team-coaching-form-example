67ab79f85deb08a3fa9f6017dec829090ebd8a72
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-03-18 17:41:40 -0400

    updated RequestButtons so all it is missing is props

--- src/components/RequestButtons.tsx
+++ src/components/RequestButtons.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import './RequestButtons.css';
 
 function RequestButtons() {
+    function handleSubmit() {
+        console.log("submit");
+    }
+
+    function handleClear() {
+        console.log("clear");
+    }
     return (
         <div className={"requestButtonsDiv"}>
+            <button onClick={handleSubmit}>Submit</button>
+            <button onClick={handleClear}>Clear</button>
         </div>
     );
 }
